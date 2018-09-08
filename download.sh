3099b9b4875b195faf542556793015f3ccc43258
Elias Ojala <git@eliasojala.me>
2018-09-08 23:41:23 +0300

    Sort to folders by channel ID

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
 SUB_LANG=en
+CHANNEL_URL="https://www.youtube.com/channel/UC7_gcs09iThXybpVgjHZ_7g"
 
 youtube-dl \
 --write-sub \
 --sub-lang $SUB_LANG \
 -f "bestvideo[height<=?1200]+bestaudio" \
--o 'videos/%(upload_date)s - %(title)s [%(resolution)s].%(ext)s' \
+-o 'videos/%(uploader_id)s/%(upload_date)s - %(title)s [%(resolution)s].%(ext)s' \
 --embed-subs \
 --sub-format ttml \
 --convert-subs srt \
@@ -13,4 +14,4 @@ youtube-dl \
 --external-downloader-args "--file-allocation=prealloc --min-split-size=1M --split=16 --max-connection-per-server=10" \
 --download-archive "_archive.txt" \
 --no-check-certificate \
-"https://www.youtube.com/channel/UC7_gcs09iThXybpVgjHZ_7g"
+$CHANNEL_URL
